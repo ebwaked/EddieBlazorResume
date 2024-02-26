410c4ded63ab5deeb872f9ff9cb1773f9e0e7d00
Eddie Waked <ebwaked@hotmail.com>
2024-02-26 03:04:27 -0500

    Added lots of content such as background and resume via iframe

---
+++ EddieBlazorResume.Client/Services/HttpClientHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EddieBlazorResume.Client.Services
+{   
+    public class HttpClientHelper
+    {
+        private readonly HttpClient _client;
+        private string? responseString = null;
+
+        public HttpClientHelper(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> HttpGetAsync(string requestUri)
+        {
+            {
+                try
+                {
+                    HttpResponseMessage response = new HttpResponseMessage();
+                    response = await _client.GetAsync(requestUri);
+                    responseString = await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
+
+            return responseString;
+        }
+    }
+
+}
