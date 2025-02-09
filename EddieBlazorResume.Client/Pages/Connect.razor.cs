7371f6281b2a4922e007df4c34bd3567da7ef87f
Eddie Waked <ebwaked@hotmail.com>
2025-02-09 02:26:22 -0500

    updating contact form

--- EddieBlazorResume.Client/Pages/Connect.razor.cs
+++ EddieBlazorResume.Client/Pages/Connect.razor.cs
@@ -4,4 +4,35 @@
     {
         // do stuff
     }
+
+    private async void SendMail()
+    {
+        emailRequest.Body = JsonConvert.SerializeObject(form.Model);
+        emailRequest.Subject = "Inquiry from DevEddie.com";
+        emailRequest.ToEmail = "eddie.waked+inquiries@gmail.com";
+
+        try
+        {
+            var response = await HttpClient.PostAsJsonAsync("SendEmail", emailRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                statusMessage = "Email sent successfully!";
+                contact.ConnectionType = null;
+                contact.Email = null;
+                contact.Message = null;
+                contact.Name = null;
+            }
+            else
+            {
+                statusMessage = $"Failed to send email. Status: {response.StatusCode}";
+            }
+
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            statusMessage = $"Error: {ex.Message}";
+        }
+    }
 }
